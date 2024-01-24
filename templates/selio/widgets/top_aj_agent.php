53ec74515f688f31d5af4f9d7fd1fd9ed26d4fc2
digiPandaAman <aman@digipanda.co.in>
2024-01-24 18:36:46 +0530

    Add New Page Section

---
+++ templates/selio/widgets/top_aj_agent.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Widget-title: Right agents
+Widget-preview-image: /assets/img/widgets_preview/right_agents.webp
+ */
+?>
+
+
+<section class="explore-feature hp7 section-padding widget_edit_enabled pb-0 mt-4 seller-bg">
+    <div class="container-fluid px-5">
+        <div class="row justify-content-center">
+            <div class="col-xl-8">
+                <div class="section-heading mb-4">
+                    <span>Popular Seller</span>
+                    <h3>Recommended Seller </h3>
+                </div>
+            </div>
+        </div>
+        <?php
+            echo "<pre>";
+            print_r($paginated_agents);
+        ?>
+        <div class="row mt-4">
+            <?php if(!empty($paginated_agents)):foreach($paginated_agents as $item): ?>
+            <div class="col-xl-3 col-sm-6 col-md-6 col-lg-4">
+                <div class="sellers_card">
+                    <div class="card-body">
+                        <div class="sellers_img">
+                            <img src="<?php echo _simg($item['image_url'], '112x89', true); ?>"
+                                alt="<?php  _che($item['name_surname']);?>">
+                        </div>
+                        <div class="sellers_detail">
+                            <div class="sellers_name_row">
+                                <h4 class="seller_name_title"><a href="<?php echo $item['agent_url']; ?>"
+                                        title="<?php  _che($item['name_surname']);?>"><?php  _che($item['name_surname']);?></a>
+                                </h4>
+                                <?php if($item['agent_profile']['activated']): ?>
+                                <span class="seller_expert">Housing Expert <b>Pro</b></span>
+                                <?php endif; ?>
+                            </div>
+                            <div class="seller_location">
+                                <span><?php  _che($item['address']);?></span>
+                            </div>
+                            <div class="seller_exp_pro">
+                                <?php
+									$OldDate = new DateTime(date('Y-m-d', strtotime($item['agent_profile']['registration_date'])));
+									$now = new DateTime(Date('Y-m-d'));
+                                    $gapDate = $OldDate->diff($now);
+                                    if($gapDate->y):
+                                        $registerText = $gapDate->y.' Year';
+                                    else:
+                                        if($gapDate->m):
+                                            $registerText = $gapDate->m.' Month';
+                                        else:
+                                            $registerText = '1 Day';
+                                        endif;
+                                    endif;
+                                ?>
+                                <span class="seller_exp"><b>Reg:</b> <?=$registerText;?> Ago</span>
+                                <span class="seller_property"><b><?php  _che($item['total_listings_num']);?></b> Properties</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; endif;?>
+        </div>
+    </div>
+</section>
+
+
+
+<!--old code -->
+<!-- <div class="widget widget-posts widget_edit_enabled widget-right-agents">
+    <h3 class="widget-title"><?php echo lang_check('Agents');?></h3>
+    <ul>
+        <?php if(!empty($paginated_agents)):foreach($paginated_agents as $item): ?>
+        <li>
+            <div class="contct-info">
+                <img src="<?php echo _simg($item['image_url'], '112x89', true); ?>"
+                    alt="<?php  _che($item['name_surname']);?>">
+                <div class="contct-nf">
+                    <h3><a href="<?php echo $item['agent_url']; ?>"
+                            title="<?php  _che($item['name_surname']);?>"><?php  _che($item['name_surname']);?></a></h3>
+                    <h4><?php  _che($item['address']);?></h4>
+                    <?php 
+                        $justNums = preg_replace("/[^0-9]/", '',  _ch($item['phone'],'#'));
+                    ?>
+                    <span><i class="la la-phone"></i><a
+                            href="tel://<?php echo $justNums;?>"><?php  _che($item['phone']);?></a></span>
+                    <span><i class="la la-envelope-o"></i><a
+                            href="mailto:<?php  _che($item['mail']);?>?subject=<?php echo urlencode(lang_check('Estateinqueryfor'));?>:<?php echo urlencode($page_title);?>"
+                            title="<?php  _che($item['mail']);?>" class=""><?php  _che($item['mail']);?></a></span>
+                </div>
+            </div>
+        </li>
+        <?php endforeach;?>
+        <?php else:?>
+        <li>
+            <div class="alert alert-success">
+                <?php echo lang_check('Not found');?>
+            </div>
+        </li>
+        <?php endif;?>
+    </ul>
+</div> -->
+<!--widget-posts end-->
--- templates/selio/widgets/top_locations_grid.php
+++ templates/selio/widgets/top_locations_grid.php
@@ -5,7 +5,7 @@ Widget-preview-image: /assets/img/widgets_preview/top_locations_grid.webp
  */
 ?>
 <?php
-$treefields = generate_treefields_list(64,'limit_3_level_0');
+$treefields = generate_treefields_list(64,'limit_3');
 ?>
 
 <?php
@@ -39,80 +39,97 @@ if(empty($treefields)) {
                 </div>
             </div>
         </div>
-		
-		<!--
-        <div class="row">
-            <!?php $i=0;  foreach ($treefields as $key=>$item): ?>
-            <!?php if ($i>=3) break; ?>
-                <div class="col-lg-4 col-md-6">
-                    <a href='<!?php _che($item['url']);?>'>
-                        <div class="card">
-                            <div class="overlay"></div>
-                            <!?php 
+
+        <!-- <div class="row">
+            <?php $i=0;  foreach ($treefields as $key=>$item): ?>
+            <?php if ($i>=3) break; ?>
+            <div class="col-lg-4 col-md-6">
+                <a href='<?php _che($item['url']);?>'>
+                    <div class="card">
+                        <div class="overlay"></div>
+                        <?php 
                                 if(!isset($defaul_images[$key])){
                                     $defaul_images[$key] = 'assets/img/no_image.jpg';
                                 }
                             ?>
-                            <img src="<!?php _che($item['thumbnail_url'], $defaul_images[$key]);?>" alt="<!?php _che($item['title']);?>" class="img-fluid">
-                            <div class="card-body">
-                                <h4><!?php _che($item['title']);?></h4>
-                                <p>
-                                    <!?php if(_ch($item['count'], 0) == 1):?>
-                                        <!?php _che($item['count'], 0);?> <!?php echo lang_check('Listing');?>                                                                    
-                                    <!?php else:?>
-                                        <!?php _che($item['count'], 0);?> <!?php echo lang_check('Listings');?>                                                                    
-                                    <!?php endif;?>
-                                </p>
-                                <i class="fa fa-angle-right"></i>
-                            </div>
+                        <img src="<?php _che($item['thumbnail_url'], $defaul_images[$key]);?>"
+                            alt="<?php _che($item['title']);?>" class="img-fluid">
+                        <div class="card-body">
+                            <h4><?php _che($item['title']);?></h4>
+                            <p>
+                                <?php if(_ch($item['count'], 0) == 1):?>
+                                <?php _che($item['count'], 0);?> <?php echo lang_check('Listing');?>
+                                <?php else:?>
+                                <?php _che($item['count'], 0);?> <?php echo lang_check('Listings');?>
+                                <?php endif;?>
+                            </p>
+                            <i class="fa fa-angle-right"></i>
                         </div>
-                    </a>
-                </div>
-            <!?php $i++; endforeach; ?>
+                    </div>
+                </a>
+            </div>
+            <?php $i++; endforeach; ?>
+        </div> -->
+
+        <!-- <div class="row mt-5">
+            <?php $i=0;  foreach ($treefields as $key=>$item): ?>
+            <?php if ($i>=3) break; ?>
+            <div class="col-lg-4 col-md-4 col-6">
+                <a href="<?php _che($item['url']);?>">
+                    <div class="card popular_locations">
+                        <div class="overlay"></div>
+                        <?php 
+                            if(!isset($defaul_images[$key])){
+                                $defaul_images[$key] = 'assets/img/no_image.jpg';
+                            }
+                        ?>
+                        <img src="<?php _che($item['thumbnail_url'], $defaul_images[$key]);?>" alt="<?php _che($item['title']);?>" class="img-fluid">
+                        <div class="card-body">
+                            <h4><?php _che($item['title']);?></h4>
+                            <p>
+                                <?php if(_ch($item['count'], 0) == 1):?>
+                                <?php _che($item['count'], 0);?> <?php echo lang_check('Listing');?>
+                                <?php else:?>
+                                <?php _che($item['count'], 0);?> <?php echo lang_check('Listings');?>
+                                <?php endif;?>
+                            </p>
+                            <i class="fa fa-angle-right"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <?php $i++; endforeach; ?>
+        </div> -->
+        
+        <div class="row mt-5">
+            <?php $i=0;  foreach ($treefields as $key=>$item): ?>
+            <?php if ($i>=3) break; ?>
+            <?php if($item['childs']): foreach($item['childs'] as $childsData): if($childsData['count']): ?>
+            <div class="col-lg-3 col-md-4 col-6 mt-3">
+                <a href="<?php _che($childsData['url']);?>">
+                    <div class="card popular_locations">
+                        <div class="overlay"></div>
+                        <?php 
+                            if(!isset($defaul_images[$key])){
+                                $defaul_images[$key] = 'assets/img/no_image.jpg';
+                            }
+                        ?>
+                        <img src="<?php _che($childsData['thumbnail_url'], $defaul_images[$key]);?>" alt="<?php _che($childsData['title']);?>" class="img-fluid">
+                        <div class="card-body">
+                            <h4><?php _che($childsData['title']);?></h4>
+                            <p>
+                                <?php if(_ch($childsData['count'], 0) == 1):?>
+                                <?php _che($childsData['count'], 0);?> <?php echo lang_check('Listing');?>
+                                <?php else:?>
+                                <?php _che($childsData['count'], 0);?> <?php echo lang_check('Listings');?>
+                                <?php endif;?>
+                            </p>
+                            <i class="fa fa-angle-right"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <?php endif; endforeach; endif; $i++; endforeach; ?>
         </div>
-	-->
-		<div class="row">
-			<div class="col-lg-4 col-md-6">
-<a href="https://digipanda.biz/realEstate/index.php/en/145/search_property">
-<div class="card">
-<div class="overlay"></div>
-<img src="https://digipanda.biz/realEstate/files/noida.jpg" alt="India" class="img-fluid">
-<div class="card-body">
-<h4>Noida</h4>
-<i class="fa fa-angle-right"></i>
-</div>
-</div>
-</a>
-</div>
-			<div class="col-lg-4 col-md-6">
-<a href="https://digipanda.biz/realEstate/index.php/en/145/search_property">
-<div class="card">
-<div class="overlay"></div>
-<img src="https://digipanda.biz/realEstate/files/gurgaon.jpg" alt="India" class="img-fluid">
-<div class="card-body">
-<h4>Gurgaon</h4>
-<i class="fa fa-angle-right"></i>
-</div>
-</div>
-</a>
-</div>
-			
-			<div class="col-lg-4 col-md-6">
-<a href="https://digipanda.biz/realEstate/index.php/en/145/search_property">
-<div class="card">
-<div class="overlay"></div>
-<img src="https://digipanda.biz/realEstate/files/dubai.jpg" alt="India" class="img-fluid">
-<div class="card-body">
-<h4>Dubai</h4>
-<i class="fa fa-angle-right"></i>
-</div>
-</div>
-</a>
-</div>
-			
-		</div>
-		
-		
-		
     </div>
 </section>
