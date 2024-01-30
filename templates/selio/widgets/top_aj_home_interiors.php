295d4fe68580fd5a3ff77eb4805bcc0194028e60
digiPandaAman <aman@digipanda.co.in>
2024-01-30 12:46:51 +0530

    add New Pages

---
+++ templates/selio/widgets/top_aj_home_interiors.php
@@ -0,0 +1,154 @@
+<style>
+    section.footer_area {
+        display: none;
+    }
+</style>
+
+<header class="text-center">
+    <a href="https://digipanda.biz/realEstate/" class="navbar-brand CusTom_Logo">
+        <img width="180" src="https://digipanda.biz/realEstate/files/logo_final.png" alt="TownManor">
+    </a>
+</header>
+<div class="AppForm">
+    <div class="header-title">
+        <div class="header-icon">
+            <i class="img-icon fa fa-file-text-o"></i>
+        </div>
+        <div class="header-lael"><label>Home Interiors Details</label></div>
+    </div>
+    <!-- <form class="form"> -->
+    <?=form_open('',['class'=>'form']);?>
+    <div class="form-group">
+
+        <input type="text" class="form-control" placeholder="">
+        <label class="label_up">Name</label>
+    </div>
+    <div class="form-group">
+
+        <input type="password" class="form-control" placeholder="">
+        <label class="label_up">Phone Numer</label>
+    </div>
+    <div class="form-group">
+
+        <input type="email" class="form-control" placeholder="">
+        <label class="label_up">Email Id</label>
+    </div>
+    <div class="form-group">
+        <select class="form-control">
+            <option selected>City</option>
+            <option>Mumbai</option>
+            <option>Bangalore</option>
+            <option>Gurgaon</option>
+            <option>Noida</option>
+            <option>Hyderabad</option>
+            <option>Pune</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <h6>Select Vendor </h6>
+        <div class="d-flex align-items-center">
+            <input type="checkbox" class="form-check" id="exampleCheck1" checked>
+            <label class="form-check position-relative m-0 pl-2" for="exampleCheck1">Livspace</label>
+        </div>
+    </div>
+    <div class="form-group">
+        <select class="form-control">
+            <option selected>Budget</option>
+            <option>1-3 lacs</option>
+            <option>3-5 lacs</option>
+            <option>5-10 lacs</option>
+            <option>10 lacs above</option>
+        </select>
+    </div>
+
+    <div class="radio-row mt-4">
+        <h6>Scope of work</h6>
+        <div class="radio-field radio-field3">
+            <input type="radio" id="Full-home-interiors" name="Scope-of-work" value="Full home interiors" checked="">
+            <label for="Full-home-interiors" id="Full-home-interiors"> Full home interiors</label>
+        </div>
+        <div class="radio-field radio-field4">
+            <input type="radio" id="Kitchen-Wardrobes" name="Scope-of-work" value="Kitchen & Wardrobes">
+            <label for="Kitchen-Wardrobes" id="Kitchen-Wardrobes">Kitchen & Wardrobes</label>
+        </div>
+        <div class="radio-field radio-field2">
+            <input type="radio" id="Only-kitchen" name="Scope-of-work" value="Only kitchen">
+            <label for="Only-kitchen" id="Only-kitchen">Only kitchen</label>
+        </div>
+        <div class="radio-field radio-field5">
+            <input type="radio" id="Other" name="Scope-of-work" value="Other">
+            <label for="Other" id="Other">Other</label>
+        </div>
+    </div>
+
+    <div class="radio-row mt-3">
+        <h6>Type of Apartment </h6>
+        <div class="radio-field radio-field3">
+            <input type="radio" id="1BHK" name="Type-of-Apartment" value="1 BHK" checked="">
+            <label for="1BHK" id="1BHK">1 BHK</label>
+        </div>
+        <div class="radio-field radio-field4">
+            <input type="radio" id="2BHK" name="Type-of-Apartment" value="2 BHK">
+            <label for="2BHK" id="2BHK">2 BHK</label>
+        </div>
+        <div class="radio-field radio-field2">
+            <input type="radio" id="3BHK" name="Type-of-Apartment" value="3 BHK">
+            <label for="3BHK" id="3BHK">3 BHK</label>
+        </div>
+        <div class="radio-field radio-field5">
+            <input type="radio" id="4BHK" name="Type-of-Apartment" value="4 BHK">
+            <label for="4BHK" id="4BHK">4 BHK</label>
+        </div>
+        <div class="radio-field radio-field5">
+            <input type="radio" id="Villa" name="Type-of-Apartment" value="Villa">
+            <label for="Villa" id="Villa">Villa</label>
+        </div>
+        <div class="radio-field radio-field5">
+            <input type="radio" id="Other1" name="Type-of-Apartment" value="Other">
+            <label for="Other1" id="Other1">Other</label>
+        </div>
+        <div class="radio-field radio-field5">
+            <input type="radio" id="Independent House" name="Type-of-Apartment" value="Independent House">
+            <label for="Independent House" id="Independent House">Independent House</label>
+        </div>
+    </div>
+
+    <div class="radio-row mt-4">
+        <h6>Possession Timeline </h6>
+        <div class="radio-field radio-field3">
+            <input type="radio" id="0-3-months" name="Possession-Timeline" value="0-3 months" checked="">
+            <label for="0-3-months" id="0-3-months"> 0-3 months</label>
+        </div>
+        <div class="radio-field radio-field4">
+            <input type="radio" id="3-6-months" name="Possession-Timeline" value="3-6 months">
+            <label for="3-6-months" id="3-6-months">3-6 months</label>
+        </div>
+        <div class="radio-field radio-field2">
+            <input type="radio" id="More-than-6-months" name="Possession-Timeline" value="More than 6 months">
+            <label for="More-than-6-months" id="More-than-6-months">More than 6 months</label>
+        </div>
+        <div class="radio-field radio-field5">
+            <input type="radio" id="Already-moved-in" name="Possession-Timeline" value="Already moved in">
+            <label for="Already-moved-in" id="Already-moved-in">Already moved in</label>
+        </div>
+    </div>
+
+    <div class="form-group">
+
+        <input type="email" class="form-control" placeholder="">
+        <label class="label_up">Any other comments</label>
+    </div>
+    <div class="form-group">
+        <div class="d-flex align-items-start">
+            <input type="checkbox" class="form-check" id="exampleCheck1" checked="">
+            <label class="form-check position-relative m-0 pl-2 font-bold" for="exampleCheck1">I hereby authorize
+                TownManor and its partners to contact me via WhatsApp, sms, phone, email etc.</label>
+        </div>
+    </div>
+
+    <div class="text-trm-condition">By submitting you agree to <a href="/terms-of-use">Terms of use </a> and<a
+            href="/terms-and-conditions"> T&amp;C</a></div>
+
+    <button type="submit" class="btn btn-primary btn-submit">Submit</button>
+    </form>
+</div>
